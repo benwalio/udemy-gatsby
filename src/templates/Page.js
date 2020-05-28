1a15874c800cdc21caa6040e54485c931f682377
ben <ben.wallace@protonmail.ch>
2020-05-28 16:50:12 -0400

    updated slices, adding contact to navbar

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 import { RichText } from "prismic-reactjs";
 import Layout from "../components/layout";
+import SliceZone from '../components/sliceZone';
 
 export const query = graphql`
   query PageQuery($id: String) {
@@ -9,12 +10,33 @@ export const query = graphql`
       allPages(id: $id) {
         edges {
           node {
-            page_title
-            content
             _meta {
               uid
               id
             }
+            body {
+              ... on PRISMIC_PageBodyCall_to_action_grid {
+                type
+                primary {
+                  section_title
+                }
+                fields {
+                  action_button
+                  action_content
+                  action_image
+                  action_title
+                  button_destination {
+                    ... on PRISMIC_Contact_page {
+                      _meta {
+                        uid
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            content
+            page_title
           }
         }
       }
@@ -29,6 +51,7 @@ const Page = (props) => {
     <Layout>
       <RichText render={pageTitle} />
       <RichText render={pageContent} />
+      <SliceZone body={props.data.prismic.allPages.edges[0].node.body} />
     </Layout>
   );
 };
